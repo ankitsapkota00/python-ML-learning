173f19ad7bcc0a103bf516938fde0bed377598fb
ankitsapkota00 <ankitsapkota00@gmail.com>
2025-06-30 12:24:49 +0545

    Add files via upload
    Tried the question, and without any hint by using my brain and trails and errors finally did it, feelling pretty proud of it, took abou half an hour of complete focus

---
+++ '''Triangle Pattern Question'''.py
@@ -0,0 +1,15 @@
+#Triangle Pattern Question
+
+'''
+Feeling proud as i did this my self,
+took about half an hour of trail and error,
+questions like what do i need and how can such 
+output be like this it prints but in second line it cant,
+so maybe if when i gets to 4 it should print 4 times then stop"
+'''
+
+for i in range(1,11):
+    for j in range(0,i):
+        print('*',end='')
+    print()
+        
